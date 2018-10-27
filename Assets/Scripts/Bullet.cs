a32e3bc45bba007f0781991d6d96583a26241b8b
timhoran20 <timhoran20+git@gmail.com>
2018-10-27 02:59:25 -0400

    health is synced

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        var hit = collision.gameObject;
+        var health = hit.GetComponent<Health>();
+        if(health != null)
+        {
+            health.TakeDamage(10);
+        }
+
+        Destroy(gameObject);
+    }
+}
